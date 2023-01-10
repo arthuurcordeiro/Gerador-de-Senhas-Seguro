c62fd20fe504138116be886298581d82322dca33
Arthur <acordeiro210691@gmail.com>
2023-01-10 19:39:21 -0300

    Implementa e cria função de Clipboard-copy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
@@ -7,5 +8,10 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', () => {
+    copy(event.target.innerHTML);
+    alert('Senha copiada!');
 });
 
